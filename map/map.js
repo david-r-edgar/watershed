1ebea33978c7fc3c7188da4e8afdbd51db6ee0e9
David Edgar <david.r.edgar@gmail.com>
2022-02-03 00:01:44 +0000

    fix road map

--- map/map.js
+++ map/map.js
@@ -12,7 +12,7 @@ function initBaseLayers () {
   return {
     light: L.tileLayer(`${serviceUrl}/Light_27700/{z}/{x}/{y}.png?key=${config.apiKey}`, { maxZoom: 20 }),
     outdoor: L.tileLayer(`${serviceUrl}/Outdoor_27700/{z}/{x}/{y}.png?key=${config.apiKey}`, { maxZoom: 20 }),
-    road: L.tileLayer(`${serviceUrl}Road_27700/{z}/{x}/{y}.png?key=${config.apiKey}`, { maxZoom: 20 }),
+    road: L.tileLayer(`${serviceUrl}/Road_27700/{z}/{x}/{y}.png?key=${config.apiKey}`, { maxZoom: 20 }),
     leisure: L.tileLayer(`${serviceUrl}/Leisure_27700/{z}/{x}/{y}.png?key=${config.apiKey}`, { maxZoom: 20 })
   }
 }
